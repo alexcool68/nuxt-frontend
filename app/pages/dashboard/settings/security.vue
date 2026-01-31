68f227f7e4a390fcbed8afcbda335430956d6c79
Alexis LEROY <alexcool68@free.fr>
2026-01-31 19:49:31 +0100

    change password

--- app/pages/dashboard/settings/security.vue
+++ app/pages/dashboard/settings/security.vue
@@ -1,26 +1,111 @@
 <script setup lang="ts">
 import * as z from "zod";
-import type { FormError } from "@nuxt/ui";
+import type { FormError, FormErrorEvent, FormSubmitEvent } from "@nuxt/ui";
 
 const passwordSchema = z.object({
-  current: z.string().min(8, "Must be at least 8 characters"),
-  new: z.string().min(8, "Must be at least 8 characters"),
+  current_password: z.string().min(8, "Must be at least 8 characters"),
+  new_password: z.string().min(8, "Must be at least 8 characters"),
 });
 
 type PasswordSchema = z.output<typeof passwordSchema>;
 
 const password = reactive<Partial<PasswordSchema>>({
-  current: undefined,
-  new: undefined,
+  current_password: undefined,
+  new_password: undefined,
 });
 
 const validate = (state: Partial<PasswordSchema>): FormError[] => {
   const errors: FormError[] = [];
-  if (state.current && state.new && state.current === state.new) {
+  if (
+    state.current_password &&
+    state.new_password &&
+    state.current_password === state.new_password
+  ) {
     errors.push({ name: "new", message: "Passwords must be different" });
   }
   return errors;
 };
+
+const toast = useToast();
+const form = useTemplateRef("form");
+
+type FieldKey = "current_password" | "new_password";
+
+interface BackendError {
+  message: string;
+  rule?: string;
+  field: FieldKey;
+}
+
+const serverErrors = reactive<Record<string, string>>({
+  current: "",
+  new: "",
+});
+
+async function onSubmit(event: FormSubmitEvent<PasswordSchema>) {
+  const config = useRuntimeConfig();
+  const baseURL = config.public.authBaseUrl;
+
+  form.value?.setErrors([]);
+
+  try {
+    await $fetch(`${baseURL}/api/auth/update-password`, {
+      headers: {
+        Authorization: `${useAuth().token.value}`,
+      },
+      method: "PATCH",
+      body: {
+        current_password: event.data.current_password || null,
+        new_password: event.data.new_password || null,
+      },
+    });
+
+    toast.add({
+      title: "Success",
+      description: "Your password have been updated.",
+      icon: "i-lucide-check",
+      color: "success",
+      duration: 3000,
+    });
+  } catch (e: any) {
+    if (e.data && e.data.errors) {
+      const fieldMap: Record<FieldKey, string> = {
+        current_password: "current",
+        new_password: "new",
+      };
+      e.data.errors.forEach((err: BackendError) => {
+        const mappedField = fieldMap[err.field] || err.field;
+        serverErrors[mappedField] = err.message;
+      });
+
+      // Optionally focus the first error field
+      const firstErrorField = Object.keys(serverErrors).find(
+        (key) => serverErrors[key],
+      );
+      if (firstErrorField) {
+        const element = document.getElementById(firstErrorField);
+        element?.focus();
+        element?.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    } else {
+      toast.add({
+        title: "Oups, something went wrong",
+        description: e.message || "An unknown error occurred.",
+        icon: "i-lucide-x",
+        color: "error",
+        duration: 3000,
+      });
+    }
+  }
+}
+
+async function onError(event: FormErrorEvent) {
+  if (event?.errors?.[0]?.id) {
+    const element = document.getElementById(event.errors[0].id);
+    element?.focus();
+    element?.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+}
 </script>
 
 <template>
@@ -30,23 +115,26 @@ const validate = (state: Partial<PasswordSchema>): FormError[] => {
     variant="subtle"
   >
     <UForm
+      ref="form"
       :schema="passwordSchema"
       :state="password"
       :validate="validate"
+      @error="onError"
+      @submit="onSubmit"
       class="flex flex-col gap-4 max-w-xs"
     >
-      <UFormField name="current">
+      <UFormField name="current" :error="serverErrors.current">
         <UInput
-          v-model="password.current"
+          v-model="password.current_password"
           type="password"
           placeholder="Current password"
           class="w-full"
         />
       </UFormField>
 
-      <UFormField name="new">
+      <UFormField name="new" :error="serverErrors.new">
         <UInput
-          v-model="password.new"
+          v-model="password.new_password"
           type="password"
           placeholder="New password"
           class="w-full"
